b6138b63be75d2afee04fbe9cdc5daf750eacca4
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-06-25 17:36:35 +0530

    Connecting to mongodb atlas

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
+import connectDB from './config/mongodb.js';
 
 // App Config
 const app = express();
@@ -15,6 +16,8 @@ app.get("/", (req, res) => {
     res.send("Api Working")
 })
 
-app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}`);
+connectDB().then(() => {
+    app.listen(port, () => {
+        console.log(`Server started at http://localhost:${port}`);
+    })
 })
