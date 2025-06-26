f69fcf334e8a0a56eab56f0ce7e5c823ed3667e4
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-06-26 15:44:17 +0530

    project create for admin

---
+++ admin/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline bg-green-300">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ admin/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
