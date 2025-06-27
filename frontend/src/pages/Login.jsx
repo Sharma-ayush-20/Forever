3294a175a20880ca3b209e2aa00806a97965c6bb
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-06-27 14:10:50 +0530

    logout functionality

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,7 +5,7 @@ import { toast } from 'react-toastify';
 
 function Login() {
 
-  const [currentState, setCurrentState] = useState('Sign Up');
+  const [currentState, setCurrentState] = useState('Login');
   const { token, setToken, navigate, backendUrl } = useContext(ShopContext)
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
