e29a1d012d7f9e73a759b6ff1a9bb8c51bc9f5f7
Sharma-ayush-20 <sharmaayush201104@gmail.com>
2025-06-27 23:38:42 +0530

    setup for order route, model, and controller added

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,48 @@
+
+//placing orders using COD Method
+
+const placeOrder = async (req, res) => {
+
+}
+
+//placing orders using Stripe Method
+
+const placeOrderStripe = async (req, res) => {
+
+}
+
+//placing orders using Razorpay Method
+
+const placeOrderRazorpay = async (req, res) => {
+
+}
+
+// All Orders data for Admin Panel
+
+const allOrders = async (req, res) => {
+
+}
+
+// user order data for frontend
+
+const userOrders = async (req, res) => {
+
+}
+
+// update orders status for Admin Panel
+
+const updateStatus = async (req, res) => {
+
+}
+
+export {
+    placeOrder,
+    placeOrderStripe,
+    placeOrderRazorpay,
+    allOrders,
+    userOrders,
+    updateStatus,
+}
+
+
+
